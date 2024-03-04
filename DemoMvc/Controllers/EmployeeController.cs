0e4efe9f2eb238d34aa537b63f1ff3ee5a9ce631
tuyen055 <142509972+tuyen055@users.noreply.github.com>
2024-03-05 00:14:21 +0700

    update slide 17 MVC_7

--- DemoMvc/Controllers/EmployeeController.cs
+++ DemoMvc/Controllers/EmployeeController.cs
@@ -7,11 +7,8 @@ public class EmployeeController : Controller{
     public IActionResult Index(){
         return View();
     }
-    [HttpPost]
 
-    public IActionResult Index(Employee emp){
-            String str = "Mã Nhân Viên: " + emp.EmployeeID + " - Tên Nhân Viên: " + emp.EmployeeName.ToUpper() + " -  Tuổi: " + emp.Tuoi + " - Lương: "+ emp.Luong;
-        ViewBag.A = str;
+    public IActionResult Welcome(){
         return View();
     }
 }
